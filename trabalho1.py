9d474d83cb95a0f99dcf86957f3a3b56bf33dc0e
ciberwizz <miguelrossis@gmail.com>
2019-03-28 10:49:16 +0000

    init

---
+++ trabalho1.py
@@ -0,0 +1,21 @@
+TAMANHO = {x:4,y:5}
+
+class peca(object):
+    self.dimensao = [1,2]
+    self.tipo ## final ; temp ; normal
+
+
+class celula(object):
+    self.tipo ## final= f ; fixa = fx ; parede = parede ; normal = n
+    self.conteudo ## = peca
+
+
+
+class node(object):
+
+
+
+
+
+
+tab = [ [celula()*x]*y
