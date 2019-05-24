2db844275f53154fa975819124a385bc5ef700e1
Bernardo Santos <bernardo.santos10@gmail.com>
2019-05-24 20:33:40 +0100

    Losses

--- trabalho2/neural.py
+++ trabalho2/neural.py
@@ -24,12 +24,6 @@ class Window(object):
         self.lines = []
         self.inputs = []
         self.outputs = []
-    
-    # def add_input(self, values):
-    #     self.inputs.append(values)
-    
-    # def add_output(self, value):
-    #     self.outputs.append(value)
 
     def add_lines(self, line):
         self.lines.append(line)
@@ -64,25 +58,12 @@ class Window(object):
 
         for i in range(len(values)):
             values[i] = values[i]/nwin
-        """ 
-        return Input(values) """
+
         return values
 
     def get_median_output(self):
         value = [float(self.outputs[0][0])]
-        # nwin = len(self.outputs)
-        # for i in range(1,nwin):
-        #     value = value + float(self.outputs[i])
-        # value = value/nwin
-
-        """         return Output(value) """
         return value
-
-        
-
-
-
-    
 
 
 class Neural_Network(object):
@@ -91,13 +72,8 @@ class Neural_Network(object):
         self.hidden_nodes = arguments['hnodes']
         
         self.weight_input = numpy.random.randn(self.n_inputs,self.hidden_nodes)
-        """ self.weight_input =  []
-        for _ in range(self.n_inputs):
-            arr = [random.random() for j in range(self.hidden_nodes)]            
-            self.weight_input.append(arr) """
 
         self.weight_hnodes = numpy.random.randn(self.hidden_nodes,1)
-        """ self.weight_hnodes = [random.random() for j in range(self.hidden_nodes)] """
 
     def sigmoid(self,x):
         return 1/(1+numpy.exp(-x))
@@ -118,11 +94,6 @@ class Neural_Network(object):
     def backward(self, input, act_output, predicted):
         self.p_error = act_output - predicted
         self.p_delta = self.p_error*self.sigmoidPrime(predicted)
-        # print("act_output",act_output)
-        # print("predicted",predicted)
-        # print("p_error",self.p_error)
-        # print("p_delta",self.p_delta)
-        # print("weight_hno", self.weight_hnodes)
 
         self.h_error = self.p_delta.dot(self.weight_hnodes.T)
         self.h_delta = self.h_error*self.sigmoidPrime(self.act_function)
@@ -159,8 +130,6 @@ In addition, gender of participant is included in the last character of file nam
 '''
 def read_data_sets():
     file_list = glob.glob(arguments['path'] + 'd?p[0-9]*[MF]')
-    #print("files",file_list)
-    #test_files = random.sample(file_list,k=int(len(file_list)*2/3))
 
     for file in file_list:
         with open(file, newline='') as csvfile:
@@ -195,24 +164,10 @@ def read_data_sets():
                     win.add_lines(inputs)
                     
                     time_slot = time_slot +1
-                    
-                    #print('time',row[0])
-                    #print('GF',row[1])
-                    #print('GV',row[2])
-                    #print('GL',row[3])
-                    #print('antenna',row[4])
-                    #print('rssi',row[5])
-                    #print('phase',row[6])
-                    #print('freq',row[7])
-                    #print('gender',file[-1:])
-                    #print('expected_output',row[8])
 
             window_list.append(win)
     
     random.shuffle(window_list)
-
-#pp = pprint.PrettyPrinter(indent=4)
-#pp.pprint(file_list)
 
 
 neural_net = Neural_Network()
@@ -242,19 +197,12 @@ if len(sys.argv) > 1:
             arr_i.append(w.get_median_inputs())
             arr_o.append(w.get_median_output())
 
-
         training_inputs = numpy.array(arr_i, dtype=float)
         training_outputs = numpy.array(arr_o, dtype=float)
-
  
         #scale
         training_inputs = training_inputs /numpy.amax(training_inputs,axis=0)
-        training_outputs = training_outputs/10#4 # val max de output
-
-        # print('neural_net: ', neural_net.forward(training_inputs)   )
-        # print('expected_output', training_outputs)
-
-
+        training_outputs = training_outputs/4 #4 # val max de output
 
 
 else:
@@ -268,8 +216,6 @@ else:
 for i in range(int(arguments['iterations'])):
     neural_net.train(training_inputs,training_outputs)
 
-print("test_window",len(test_window))
-
 arr_i = []
 arr_o = []
 for w in test_window:
@@ -282,11 +228,14 @@ test_output = numpy.array(arr_o, dtype=float)
 
  #scale
 test_input = test_input /numpy.amax(test_input,axis=0)
-test_output = test_output/10#4 # val max de output
+test_output = test_output/4 #4 # val max de output
 
 
 predicted_out = neural_net.forward(test_input)
 
 for i in range(len(test_output)):
-    print("actual = ", test_output[i]," predicted = ",predicted_out[i])
+    print("actual = ", 4*test_output[i]," predicted = ",4*predicted_out[i])
+
+print("Training Loss: ", str(numpy.mean(numpy.square(training_outputs - neural_net.forward(training_inputs)))))
+print("Testing Loss: ", str(numpy.mean(numpy.square(test_output - predicted_out))))
 
